a2199efe426174b889fb48901ffad0eafc88783b
mouleee-19 <moulikuruba98488@gmail.com>
2025-10-24 00:52:37 +0530

    Time: 6 ms (79.48%), Space: 12.4 MB (81.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # Negative numbers can't be palindromes
+        if x < 0:
+            return False
+        
+        # Convert to string and compare with its reverse
+        s = str(x)
+        return s == s[::-1]
